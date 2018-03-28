0a3872ef78237773ecd0cc6941a3dd88ca1042c5
ghumman3131 <charanghumman@gmail.com>
2018-03-28 15:03:40 +0530

    read data

--- app/src/main/java/com/inception/firebaseauthentication/UserProfile.java
+++ app/src/main/java/com/inception/firebaseauthentication/UserProfile.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -13,11 +16,7 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-
-
-
-
-
+        read_profile_data();
 
 
     }
@@ -30,7 +29,37 @@ public class UserProfile extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
 
 
-        database.getReference().child("charan@gmailcom").setValue(data);
+        database.getReference().child("user").child("xyz@gmailcom").setValue(data);
 
     }
+
+    public void read_profile_data()
+    {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        ValueEventListener event_listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                ProfileData recieved_data = dataSnapshot.getValue(ProfileData.class);
+
+                System.out.println( recieved_data.name );
+                System.out.println( recieved_data.age );
+                System.out.println( recieved_data.address );
+                System.out.println( recieved_data.gender );
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+
+        database.getReference().child("user").child("xyz@gmailcom").addValueEventListener(event_listener);
+
+    }
+
 }
